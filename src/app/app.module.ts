a99e8b547500b04a4529fcb31416183e7d69f4ce
srikanth461 <srikanth.n24@gmail.com>
2024-09-28 08:36:03 +0530

    Angular18 lifecycle hooks

---
+++ src/app/app.module.ts
@@ -0,0 +1,73 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+
+import {
+  AfterContentParentComponent,
+  AfterContentComponent,
+  ChildComponent
+} from './after-content.component';
+
+import {
+  AfterViewParentComponent,
+  AfterViewComponent,
+  ChildViewComponent
+} from './after-view.component';
+
+import {
+  CounterParentComponent,
+  MyCounterComponent
+} from './counter.component';
+
+import {
+  DoCheckParentComponent,
+  DoCheckComponent
+} from './do-check.component';
+
+import {
+  OnChangesParentComponent,
+  OnChangesComponent
+} from './on-changes.component';
+
+import { PeekABooParentComponent } from './peek-a-boo-parent.component';
+import { PeekABooComponent } from './peek-a-boo.component';
+
+import { SpyParentComponent } from './spy.component';
+import { SpyDirective } from './spy.directive';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [
+    AppComponent,
+    AfterContentParentComponent,
+    AfterContentComponent,
+    ChildComponent,
+    AfterViewParentComponent,
+    AfterViewComponent,
+    ChildViewComponent,
+    CounterParentComponent,
+    MyCounterComponent,
+    DoCheckParentComponent,
+    DoCheckComponent,
+    OnChangesParentComponent,
+    OnChangesComponent,
+    PeekABooParentComponent,
+    PeekABooComponent,
+    SpyParentComponent,
+    SpyDirective
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
+
+
+/*
+Copyright 2017-2018 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
---
+++ src/app/spy.directive.ts
@@ -0,0 +1,28 @@
+import { Directive, OnInit, OnDestroy } from '@angular/core';
+
+import { LoggerService } from './logger.service';
+
+let nextId = 1;
+
+// Spy on any element to which it is applied.
+// Usage: <div mySpy>...</div>
+@Directive({selector: '[mySpy]'})
+export class SpyDirective implements OnInit, OnDestroy {
+
+  constructor(private logger: LoggerService) { }
+
+  ngOnInit()    { this.logIt(`onInit`); }
+
+  ngOnDestroy() { this.logIt(`onDestroy`); }
+
+  private logIt(msg: string) {
+    this.logger.log(`Spy #${nextId++} ${msg}`);
+  }
+}
+
+
+/*
+Copyright 2017-2018 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
